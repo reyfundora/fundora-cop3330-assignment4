fe8399bc18abd9a75e59582651950312e768c70a
Rey <reyfundora@icloud.com>
2021-07-05 18:02:16 -0400

    commit

--- src/main/java/ucf/assignments/File_Open.java
+++ src/main/java/ucf/assignments/File_Open.java
@@ -4,6 +4,4 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class File_Open {
-
-
 }
--- src/main/java/ucf/assignments/Todo_List_App.java
+++ src/main/java/ucf/assignments/Todo_List_App.java
@@ -1,4 +1,66 @@
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
 public class Todo_List_App {
+
+    public MenuItem File_Open, File_Save_Single, File_Save_All, File_Close, Edit_New_List, Edit_New_Item,
+                    View_All, View_Completed, View_Incompleted, Help_Getting_Started, Help_About,
+                    RC_List_Rename, RC_List_Delete, RC_Item_MComplete, RC_Item_MIncomplete, RC_Item_Rename,
+                    RC_Item_Date, RC_Item_Delete;
+
+    public void Open_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void Save_Single_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void Save_All_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void Close_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void New_List_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void New_Item_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void All_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void Completed_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void Incompleted_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void Started_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void About_Button_Click(ActionEvent actionEvent) {
+    }
+
+    public void list_rename_button_click(ActionEvent actionEvent) {
+    }
+
+    public void list_delete_button_click(ActionEvent actionEvent) {
+    }
+
+    public void mark_com_button_click(ActionEvent actionEvent) {
+    }
+
+    public void mark_incom_button_click(ActionEvent actionEvent) {
+    }
+
+    public void item_rename_button_click(ActionEvent actionEvent) {
+    }
+
+    public void item_redate_button_click(ActionEvent actionEvent) {
+    }
+
+    public void item_delete_button_click(ActionEvent actionEvent) {
+    }
 }
