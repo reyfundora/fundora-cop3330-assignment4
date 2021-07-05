6d5315b363c14feaca2ec4c7993008bc9bcd935e
Rey <reyfundora@icloud.com>
2021-07-05 13:20:39 -0400

    commit

---
+++ src/main/java/ucf/assignments/File.java
@@ -0,0 +1,32 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class File {
+
+    @FXML
+    void Close_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void New_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Open_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Save_All_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Save_Single_Button(ActionEvent event) {
+
+    }
+}
